5119e01dfd0cbae4db68701eb8ff81adc5ebd694
AlfaOmegaX <miguel_lb_97@hotmail.com>
2016-03-15 22:59:59 +0100

    Minnor fixes to bounding_box.cpp

--- 1_gestion_proyectos/src/bounding_box.cpp
+++ 1_gestion_proyectos/src/bounding_box.cpp
@@ -8,19 +8,18 @@
 #include <fstream>
 #include <limits>
 #include "rectangle.h"
-
 using namespace std;
 
 /**
   * @brief Elimina comentarios de un archivo de cara a una posterior lectura del contenido
   * @pre Los comentarios comienzan con "delim" y acaban al final de línea
   */
-void EraseComment ( istream& is, char delim )
+void EraseComment( istream& is, char delim )
 {
   // Avanzamos hasta encontrar algo distinto a un blanco o el carácter "delim"
-  while ( isspace( is.peek() ) || is.peek() == delim )
+  while( isspace( is.peek() ) || is.peek() == delim )
   {
-    if ( is.peek() == delim )
+    if( is.peek() == delim )
       is.ignore( 1024, '\n' );  // Suponemos que las líneas tienen menos de 1024 caracteres
     else
       is.ignore();
@@ -31,7 +30,7 @@ void EraseComment ( istream& is, char delim )
   * @brief Calcula el rectángulo delimitador más pequeño de una nube de puntos
   * @param Flujo de entrada de datos (is)
   */
-Rectangle BoundingBox ( istream& is )
+Rectangle BoundingBox( istream& is )
 {
   Point p, p_min, p_max;
   Rectangle bounding;
@@ -68,14 +67,15 @@ int main( int argc, char* argv[] )
   Rectangle bounding;
   bool input_finished;
 
-  if ( argc == 1 ) { // Si no se ha introducido el nombre del archivo como parámetro
+  if( argc == 1 ) { // Si no se ha introducido el nombre del archivo como parámetro
     bounding = BoundingBox( cin );
     input_finished = cin.eof();   // Comprueba si se ha leído hasta el final
   }
-  else {
+  else
+  {
     ifstream f( argv[1] );
 
-    if ( !f ) {
+    if( !f ) {
       cerr << "No se puede abrir el archivo " << argv[1] << endl;
       return 1;   // Forzamos el cierre, devolviendo al sistema un error
     }
@@ -84,7 +84,8 @@ int main( int argc, char* argv[] )
     input_finished = f.eof();
   }
 
-  if ( !input_finished ) {
+  if( !input_finished )
+  {
     cerr << "Error inesperado: no se han podido leer todos los datos." << endl;
     return 1;
   }
