ba9478d373e75b93a8318989ebc57cdab7ebc1c3
AlfaOmegaX <miguel_lb_97@hotmail.com>
2016-03-15 22:53:24 +0100

    Minnor fixes to circle.cpp/.h

--- 1_gestion_proyectos/include/circle.h
+++ 1_gestion_proyectos/include/circle.h
@@ -13,7 +13,8 @@
 // ------------------------------------------
 
 /// Representa un círculo en 2D, a través de su centro y su radio
-struct Circle {
+struct Circle
+{
     Point center;
     double radius;
 };
@@ -26,7 +27,7 @@ struct Circle {
   * y leyendo "centro" con ReadPoint
   * @deprecated Se aconseja el uso de la función con un parámetro
   */
-Circle ReadCircle ();
+Circle ReadCircle();
 
 /**
   * @brief Lee un círculo desde un flujo de entrada, usando ReadPoint para leer el centro.
@@ -34,14 +35,14 @@ Circle ReadCircle ();
   * @pre El formato debe ser radio-(x,y), donde (x,y) es el centro
   * @return Devuelve si ha tenido éxito la lectura
   */
-bool ReadCircle ( std::istream& is, Circle& c );
+bool ReadCircle( std::istream& is, Circle& c );
 
 /**
   * @brief Escribe círculo a la salida estándar, en formato radio-centro, incluyendo '-'
   * y escribiendo "centro" con WritePoint
   * @deprecated Se aconseja el uso de la función con dos parámetros
   */
-void WriteCircle ( const Circle& c );
+void WriteCircle( const Circle& c );
 
 /**
   * @brief Escribe un círculo a un flujo de salida, en formato radio-centro, escribiendo
@@ -49,24 +50,24 @@ void WriteCircle ( const Circle& c );
   * @param Flujo de salida de datos (os), círculo a escribir (c)
   * @return Devuelve si ha tenido éxito la escritura
   */
-bool WriteCircle ( std::ostream& os, const Circle& c );
+bool WriteCircle( std::ostream& os, const Circle& c );
 
 /// Inicializa circulo c con centro y radio
-void InitCircle ( Circle& c, const Point& center, double radius );
+void InitCircle( Circle& c, const Point& center, double radius );
 
 /// Devuelve el centro de c
-Point GetCenter ( const Circle& c );
+Point GetCenter( const Circle& c );
 
 /// Devuelve el radio de c
-double GetRadius ( const Circle& c );
+double GetRadius( const Circle& c );
 
 /// Devuelve el área del círculo c
-double Area ( const Circle& c );
+double Area( const Circle& c );
 
 /// Devuelve si p está en el interior del círculo c (distancia al centro menor que el radio)
-bool IsWithin ( const Point& p, const Circle& c );
+bool IsWithin( const Point& p, const Circle& c );
 
 /// Devuelve la distancia de c1 a c2 (distancia entre los centros restándole los radios)
-double Distance ( const Circle& c1, const Circle& c2 );
+double Distance( const Circle& c1, const Circle& c2 );
 
 #endif
--- 1_gestion_proyectos/src/circle.cpp
+++ 1_gestion_proyectos/src/circle.cpp
@@ -7,10 +7,9 @@
 #include <iostream>
 #include <cmath> // M_PI
 #include "circle.h"
-
 using namespace std;
 
-Circle ReadCircle ()
+Circle ReadCircle()
 {
   Circle c;
   char f;
@@ -21,14 +20,12 @@ Circle ReadCircle ()
   return c;
 }
 
-bool ReadCircle ( istream& is, Circle& c )
+bool ReadCircle( istream& is, Circle& c )
 {
   char f;
   double r;
   Point p;
-  bool success;
-
-  success = ( is >> r >> f ) && ( f == '-' ) && ReadPoint( is, p );
+  bool success = ( is >> r >> f ) && ( f == '-' ) && ReadPoint( is, p );
 
   if ( success )
     InitCircle( c, p, r );
@@ -36,46 +33,46 @@ bool ReadCircle ( istream& is, Circle& c )
   return success;
 }
 
-void WriteCircle ( const Circle& c )
+void WriteCircle( const Circle& c )
 {
   cout << c.radius << "-";
   WritePoint( c.center );
 }
 
-bool WriteCircle ( ostream& os, const Circle& c )
+bool WriteCircle( ostream& os, const Circle& c )
 {
   os << c.radius << "-";
 
-  return WritePoint ( os, c.center );
+  return WritePoint( os, c.center );
 }
 
-void InitCircle ( Circle& c, const Point& center, double radius )
+void InitCircle( Circle& c, const Point& center, double radius )
 {
   c.radius = radius;
   c.center = center;
 }
 
-Point GetCenter ( const Circle& c )
+Point GetCenter( const Circle& c )
 {
   return c.center;
 }
 
-double GetRadius ( const Circle& c )
+double GetRadius( const Circle& c )
 {
   return c.radius;
 }
 
-double Area ( const Circle& c )
+double Area( const Circle& c )
 {
   return c.radius * c.radius * M_PI;
 }
 
-bool IsWithin ( const Point& p, const Circle& c )
+bool IsWithin( const Point& p, const Circle& c )
 {
   return Distance( p, c.center ) < c.radius;
 }
 
-double Distance ( const Circle& c1, const Circle& c2 )
+double Distance( const Circle& c1, const Circle& c2 )
 {
   return Distance( c1.center, c2.center ) - ( c1.radius + c2.radius );
 }
