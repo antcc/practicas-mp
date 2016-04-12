41f67d366d1128f0da9e44f5d8f0add8434adecf
Mike <miguel_lb_97@hotmail.com>
2016-04-12 23:24:27 +0200

    Update matriz_bit.cpp

--- 3_matriz_booleanos/src/matriz_bit.cpp
+++ 3_matriz_booleanos/src/matriz_bit.cpp
@@ -8,11 +8,11 @@
 
 #include "matriz_bit.h" // Aquí se define el valor de CUAL_COMPILO
 
-#if CUAL_COMPILO==1
+#if CUAL_COMPILO == 1
   #include "matriz_bit1.cpp"
-#elif CUAL_COMPILO==2
+#elif CUAL_COMPILO == 2
   #include "matriz_bit2.cpp"
-#elif CUAL_COMPILO==3
+#elif CUAL_COMPILO == 3
   #include "matriz_bit3.cpp"
 #else
   #include "matriz_bit4.cpp"
