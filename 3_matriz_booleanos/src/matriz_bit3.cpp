00d4cbec821b0883df78a43da903d0166c625022
Mike <miguel_lb_97@hotmail.com>
2016-04-13 01:57:26 +0200

    \0 por 0

--- 3_matriz_booleanos/src/matriz_bit3.cpp
+++ 3_matriz_booleanos/src/matriz_bit3.cpp
@@ -49,7 +49,7 @@ void SetElemento (MatrizBit& m, int f, int c, bool v)
   const int FILS = GetFilas(m);
 
   if (f < FILS && c < COLS && f >= 0 && c >= 0)
-    m.v[COLS * f + c] = v ? '1' : '\0';
+    m.v[COLS * f + c] = v ? '1' : '0';
 }
 
 /* Fin fichero: matriz_bit3.cpp */
